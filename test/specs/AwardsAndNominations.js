8824588e1d5e17bf1d50c673bdc9a016ba54002f
Borysov Kostyantyn <koss199710@gmail.com>
2022-10-27 21:05:07 +0300

    Fix wdio

--- test/specs/AwardsAndNominations.js
+++ test/specs/AwardsAndNominations.js
@@ -1,4 +1,6 @@
 const MainPage = require('../pageobjects/main.page');
+const TelnyxPage = require('../pageobjects/telnyx_page');
+const Base = require('../pageobjects/base');
 
 describe("Awards and nominations for 2022 year TC-003", () => {
   it('Under the heading text "Momentum leader and highest satisfaction in G2 CPaaS grid" should be a list of awards', async () => {
