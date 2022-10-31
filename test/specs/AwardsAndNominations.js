19bbe4ecd0c5475c53fe02fc96eeab56f046eb6b
Borysov Kostyantyn <koss199710@gmail.com>
2022-10-31 18:55:58 +0200

    Fix test

--- test/specs/AwardsAndNominations.js
+++ test/specs/AwardsAndNominations.js
@@ -6,7 +6,7 @@ describe("Awards and nominations for 2022 year TC-003", () => {
   it('Under the heading text "Momentum leader and highest satisfaction in G2 CPaaS grid" should be a list of awards', async () => {
     await TelnyxPage.open('');
     await Base.cookiesButton.click();
-    MainPage.listOfAwards.scrollIntoView();
+    await MainPage.listOfAwards.scrollIntoView();
     await expect(MainPage.listOfAwards).toBeDisplayed();
     await expect(MainPage.listOfAwardsBestResults).toBeDisplayed();
     await expect(MainPage.listOfAwardsBestUsability).toBeDisplayed();
